5cb2d39532a4b5f6b01b3e771dc2b1c814561806
Ankit Goyal <ankit3goyal@gmail.com>
2014-07-20 22:47:54 -0700

    removing pry

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require 'yaml'
 #here you can load yaml files in config folder
 projects_path = File.expand_path(File.join(File.dirname(__FILE__), 'projects.yml'))
 PROJECTS = YAML.load_file(projects_path)
-require 'pry'; binding.pry
